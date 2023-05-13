20c6c2cd6ccc9a64831c859c16ebc6e00360044d
developer006 <developer.ludovic@gmail.com>
2023-05-13 20:42:54 +0300

    add default profile photo

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -169,7 +169,7 @@
             @endphp
 
         <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-          <img src="{{asset("uploads/student/$photo ?? 'default.png'")}}" class="user-image img-circle elevation-2" alt="User Image">
+          <img src="{{asset("uploads/student/$photo")}}" class="user-image img-circle elevation-2" alt="User Image">
           <span class="d-none d-md-inline">{{Auth::user()->name}}</span>
         </a>
         <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right" style="left: inherit; right: 0px;">
